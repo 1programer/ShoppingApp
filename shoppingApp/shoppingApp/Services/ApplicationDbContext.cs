5aeb09c5c5e936c6fc1ba29fe0f4de5a1c09d71a
1programer <katsaravalado@gmail.com>
2024-06-16 19:47:00 +0400

    firstcommit

---
+++ shoppingApp/shoppingApp/Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shoppingApp.Models;
+
+namespace shoppingApp.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
